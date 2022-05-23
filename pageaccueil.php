0f4b7dfcff1e7a3bb597fc01d3dc2e0a428a5c49
Zinedine <70756158+Chelgham-Zinedine@users.noreply.github.com>
2022-05-23 21:00:20 +0200

    Update pageaccueil.php

--- pageaccueil.php
+++ pageaccueil.php
@@ -8,20 +8,13 @@
   <link rel="stylesheet" href="css/inscription.css" />
   <link rel="stylesheet" href="css/media.css">
   <link rel="stylesheet" href="css/reset.css">
-
 </head>
 <body>
-<?php
-if(isset($_SESSION['email'])){
-?>
-<a href="member.php"><button type="submit" name="memberpage" class="registerbtn">Member page</button></a>
-<a href="deconnexion.php"><button type="submit" name="deconnexion" class="registerbtn">Log out</button></a>
-<?php
-}
-else
-{
-	header('Location: index.php');
-}
-?>
+	<?php if(isset($_SESSION['email'])){ ?>
+	<a href="member.php"><button type="submit" name="memberpage" class="registerbtn">Member page</button></a>
+	<a href="deconnexion.php"><button type="submit" name="deconnexion" class="registerbtn">Log out</button></a>
+	<?php }
+	else header('Location: index.php');	
+	?>
 </body>
 </html>
